561c62e66cb5eb30b0c550d652339fbb6b906a99
oran_keeling <oran_keeling@192.168.0.73>
2019-10-07 02:37:02 +0100

    Created constructor

--- src/LCA.java
+++ src/LCA.java
@@ -1,5 +1,16 @@
 // Implement a function that can calculate the lowest common ancestor in a graph, that may be structured as a binary tree.
 
+class Node {
+	int data;
+	Node left;
+	Node right;
+	
+	Node(int value) {
+		data = value;
+		left = right = null;
+	}
+}
+
 public class LCA {
 
 }
