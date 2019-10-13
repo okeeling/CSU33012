638fbf9bd814b3447bac052990e145ddaaf7c1bf
oran_keeling <oran_keeling@192.168.0.73>
2019-10-13 23:29:20 +0100

    Completed findLCA function

--- src/LCA.java
+++ src/LCA.java
@@ -21,37 +21,46 @@ public class LCA {
 	List<Integer> nodeTwoPath = new ArrayList<>();
 	
 	private int findLCA(Node root, int nodeOne, int nodeTwo) {
-		return -1;
+		if (!findPath(root, nodeOne, nodeOnePath) || !findPath(root, nodeTwo, nodeTwoPath)) {
+			if(nodeOnePath.size() == 0 && nodeTwoPath.size() == 0) {
+				if(!nodeOnePath.contains(nodeOne) && !nodeTwoPath.contains(nodeTwo)) {
+					System.out.println("These nodes do not exist in the tree.");
+				} else {
+					System.out.println("The tree is empty.");
+				}
+			}
+			if (nodeOnePath.size() > 0 && nodeTwoPath.size() == 0) {
+				System.out.println("Node 1 is present but Node 2 is not present in the tree.");
+			}
+			if (nodeTwoPath.size() > 0 && nodeOnePath.size() == 0) {
+				System.out.println("Node 2 is present but Node 1 is not present in the tree.");
+			}
+		}
+		int index;
+		for (index = 0; index < nodeOnePath.size() && index < nodeTwoPath.size(); index++) {
+			if (!nodeOnePath.get(index).equals(nodeTwoPath.get(index))) {
+				break;
+			}
+		}
+		return nodeOnePath.get(index - 1);
 	}
 	
 	private boolean findPath(Node root, int num, List<Integer> path) {
-		
-		if(root == null) {
-			
-			return false;
-			
-		} else { 
-			
+		if (root == null) {
+			return false;	
+		} else {
 			path.add(root.data);
-			
 			if (root.data == num) {
 				return true;
 			}
-			
 			if (root.left != null) {
 				findPath(root.left, num, path);
 			}
-			
 			if (root.right != null) {
 				findPath(root.right, num, path);
 			}
-			
 			path.remove(path.size() - 1);
-			
 			return false;
-			
 		}
-		
 	}
-	
 }
