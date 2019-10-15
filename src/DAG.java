cf33e0628c52aee5b194e3c61dcb58227ae78823
oran_keeling <oran_keeling@10.6.66.153>
2019-10-15 18:59:39 +0100

    NodeDAG class

--- src/DAG.java
+++ src/DAG.java
@@ -1,3 +1,17 @@
+import java.util.ArrayList;
+
+class NodeDAG {
+	int data;
+	NodeDAG left;
+	NodeDAG right;
+	ArrayList<NodeDAG> ancestors; 
+	 
+	NodeDAG(int value) {
+		data = value;
+		left = right = null;
+	  	ancestors = null;
+	}
+}
 
 public class DAG {
 
