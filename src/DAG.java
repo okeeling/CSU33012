0a94694a386dab32c76f7606f1b7bf18dd2571e1
oran_keeling <oran_keeling@10.6.89.54>
2019-10-16 22:43:30 +0100

    Completed addToGraph and addAncestorsToNode functions

--- src/DAG.java
+++ src/DAG.java
@@ -20,7 +20,7 @@ public class DAG {
 	List<Integer> nodeTwoPath = new ArrayList<>();
 	
 	private int findLCADAG(NodeDAG root, int nodeOne, int nodeTwo) {
-		if (!findPathDAG(root, nodeOne, nodeOnePath) || !findPathDAG(root, nodeTwo, nodeTwoPath)) {
+		if(!findPathDAG(root, nodeOne, nodeOnePath) || !findPathDAG(root, nodeTwo, nodeTwoPath)) {
 			if(nodeOnePath.size() == 0 && nodeTwoPath.size() == 0) {
 				if(!nodeOnePath.contains(nodeOne) && !nodeTwoPath.contains(nodeTwo)) {
 					System.out.println("These nodes do not exist in the tree.");
@@ -28,17 +28,17 @@ public class DAG {
 					System.out.println("The tree is empty.");
 				}
 			}
-			if (nodeOnePath.size() > 0 && nodeTwoPath.size() == 0) {
+			if(nodeOnePath.size() > 0 && nodeTwoPath.size() == 0) {
 				System.out.println("Node 1 is present but Node 2 is not present in the tree.");
 			}
-			if (nodeTwoPath.size() > 0 && nodeOnePath.size() == 0) {
+			if(nodeTwoPath.size() > 0 && nodeOnePath.size() == 0) {
 				System.out.println("Node 2 is present but Node 1 is not present in the tree.");
 			}
 			return -1;
 		}
 		int index;
-		for (index = 0; index < nodeOnePath.size() && index < nodeTwoPath.size(); index++) {
-			if (!nodeOnePath.get(index).equals(nodeTwoPath.get(index))) {
+		for(index = 0; index < nodeOnePath.size() && index < nodeTwoPath.size(); index++) {
+			if(!nodeOnePath.get(index).equals(nodeTwoPath.get(index))) {
 				break;
 			}
 		}
@@ -61,6 +61,29 @@ public class DAG {
 			}
 			path.remove(path.size() - 1);
 			return false;
+		}
+	}
+	
+	public void addToGraph(NodeDAG nodeOne) {
+		nodeOne.ancestors = new ArrayList<NodeDAG>();
+		nodeOne.ancestors.add(nodeOne);
+	}
+	
+	public void addAncestorsToNode(NodeDAG nodeOne, NodeDAG nodeTwo) {
+		int index;
+		for(index = 0; index < nodeOne.ancestors.size(); index++) {
+			if(!nodeTwo.ancestors.contains(nodeOne.ancestors.get(index))) {
+				nodeTwo.ancestors.add(nodeOne.ancestors.get(index));
+			}
+		}
+	 }
+	 
+	public void addAncestorToNode2(NodeDAG nodeOne, NodeDAG nodeTwo, int position) {
+		int index;
+		for(index=0; index < nodeOne.ancestors.size(); index++) {
+			if(!nodeTwo.ancestors.contains(nodeOne.ancestors.get(index))) {
+				nodeTwo.ancestors.add(position, nodeOne.ancestors.get(index));
+			}
 		}
 	}
 			
